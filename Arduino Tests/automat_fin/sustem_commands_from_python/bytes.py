cc86c714ccb8a8db4a3501b16cd58b2c86d688b8
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2021-01-24 19:43:42 +0500

    --

--- Arduino Tests/automat_fin/sustem_commands_from_python/bytes.py
+++
@@ -1,8 +0,0 @@
-from serial import Serial
-
-arduino = Serial('/dev/ttyUSB0', 9600, timeout =1)
- 
-while 1:
-    x = 600
-    arduino.write(x.to_bytes(length=2, byteorder='little'))
-    print (arduino.read())
