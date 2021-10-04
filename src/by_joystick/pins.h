dd7f253f5fabc2002d69fddfa1f60b8f5001e564
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2021-10-04 12:08:14 +0500

    rename

---
+++ src/by_joystick/pins.h
@@ -0,0 +1,18 @@
+#define led5 7
+#define led4 6
+#define led3 5
+#define led2 4
+#define led1 A4
+
+#define y    A7
+#define x    A6
+#define knok A5
+
+#define mot1_en   53
+#define mot1_dir  51
+#define mot1_step 49
+
+#define mot2_5v   47
+#define mot2_en_  35
+#define mot2_cw   33
+#define mot2_clk  45
